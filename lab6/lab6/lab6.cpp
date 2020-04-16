6ead56132df4597c29fae650ec11602a55d3d609
KateChizhevskaya <Katarinachig16@gmail.com>
2020-04-16 20:51:32 +0300

    add lab 6

---
+++ lab6/lab6/lab6.cpp
@@ -0,0 +1,256 @@
+﻿// lab6.cpp : Определяет точку входа для приложения.
+//
+
+#include "framework.h"
+#include "lab6.h"
+
+#define MAX_LOADSTRING 100
+
+// Глобальные переменные:
+int x;
+int y;
+int move_timer;
+bool paused = false;// стоит на паузе
+int startPosition; //сохранение начального положение текста
+int windowLength; // ширина окна
+PAINTSTRUCT *ps;
+HDC hdc;
+LPCWSTR text = L"Hello";
+LPCWSTR font = L"Times New Roman";
+LPCWSTR spases = L"     ";
+HINSTANCE hInst;                                // текущий экземпляр
+WCHAR szTitle[MAX_LOADSTRING];                  // Текст строки заголовка
+WCHAR szWindowClass[MAX_LOADSTRING];            // имя класса главного окна
+LPCWSTR iconName  = L"IDI_ICON1";
+LPCWSTR cursorName = L"IDC_CURSOR2";
+// Отправить объявления функций, включенных в этот модуль кода:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    // TODO: Разместите код здесь.
+
+    // Инициализация глобальных строк
+    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_LAB6, szWindowClass, MAX_LOADSTRING);
+    MyRegisterClass(hInstance);
+
+    // Выполнить инициализацию приложения:
+    if (!InitInstance (hInstance, nCmdShow))
+    {
+        return FALSE;
+    }
+
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_LAB6));
+
+    MSG msg;
+
+    // Цикл основного сообщения:
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
+    return (int) msg.wParam;
+}
+
+//
+//  ФУНКЦИЯ: MyRegisterClass()
+//
+//  ЦЕЛЬ: Регистрирует класс окна.
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc    = WndProc;
+    wcex.cbClsExtra     = 0;
+    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_ICON1));
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hCursor        = LoadCursor(nullptr, IDC_CROSS);
+    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_LAB6);
+    wcex.lpszClassName  = szWindowClass;
+    wcex.hIconSm        = LoadIcon(hInstance, iconName);
+
+    return RegisterClassExW(&wcex);
+}
+
+//
+//   ФУНКЦИЯ: InitInstance(HINSTANCE, int)
+//
+//   ЦЕЛЬ: Сохраняет маркер экземпляра и создает главное окно
+//
+//   КОММЕНТАРИИ:
+//
+//        В этой функции маркер экземпляра сохраняется в глобальной переменной, а также
+//        создается и выводится главное окно программы.
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   hInst = hInstance; // Сохранить маркер экземпляра в глобальной переменной
+
+   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+
+   if (!hWnd)
+   {
+      return FALSE;
+   }
+
+   ShowWindow(hWnd, nCmdShow);
+   UpdateWindow(hWnd);
+   return TRUE;
+}
+
+//
+//  ФУНКЦИЯ: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  ЦЕЛЬ: Обрабатывает сообщения в главном окне.
+//
+//  WM_COMMAND  - обработать меню приложения
+//  WM_PAINT    - Отрисовка главного окна
+//  WM_DESTROY  - отправить сообщение о выходе и вернуться
+//
+//
+
+void drow(HDC hdc)
+{
+	int length = strlen("Hello") + 3;
+	TextOut(hdc, x, y, spases, length); //стираем старый текст кол-во пробелов равно кол-ву букв в слове 
+	x = x + 10; //устанавливаем новые координаты 
+	TextOut(hdc, x, y, text, length); //выводим текст
+}
+
+void getStartPosition(HWND hWnd)
+{
+	RECT window; // получаем размеры окна
+	GetWindowRect(hWnd, &window);
+	x = window.left;
+	windowLength = window.right;
+	startPosition = x;
+	y = (window.bottom - window.top) / 2;
+}
+
+void endDrow(HWND hWnd, PAINTSTRUCT *ps)
+{
+	EndPaint(hWnd, ps);
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+
+    switch (message)
+    {
+
+		case WM_CREATE:
+		{
+			WM_PAINT;
+			getStartPosition(hWnd);
+			move_timer = SetTimer(hWnd, 1, 100, NULL);
+		}
+
+		case WM_COMMAND:
+			{
+				int wmId = LOWORD(wParam);
+				// Разобрать выбор в меню:
+				switch (wmId)
+				{
+				case IDM_ABOUT:
+					DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+					break;
+				case IDM_EXIT:
+					DestroyWindow(hWnd);
+					break;
+				case ID_CONTROL_START:
+				{
+					if (paused)
+					{
+						paused = false;
+						move_timer = SetTimer(hWnd, 1, 100, NULL);
+					}
+					break;
+				}
+				case ID_CONTROL_STOP:
+				{
+					if (!paused)
+					{
+						paused = true;
+						KillTimer(hWnd, move_timer);
+					}
+					break;
+				}
+				default:
+					return DefWindowProc(hWnd, message, wParam, lParam);
+				}
+			}
+			break;
+
+		case WM_TIMER:
+		{
+			if (x > windowLength - 250)
+			{
+				int length = strlen("Hello") + 3;
+				TextOut(hdc, x, y, spases, length);
+				x = startPosition;
+			}
+			drow(hdc);
+			return 0;
+		}
+
+		case WM_PAINT:
+		{
+			ps = new PAINTSTRUCT;
+			hdc = BeginPaint(hWnd, ps);			
+			return 0;
+		}
+		break;
+		case WM_DESTROY:
+		{
+			endDrow(hWnd, ps);
+			PostQuitMessage(0);
+		}
+			break;
+		default:
+			return DefWindowProc(hWnd, message, wParam, lParam);
+		}
+		return 0;
+}
+
+
+// Обработчик сообщений для окна "О программе".
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
---
+++ lab6/lab6/resource.h
@@ -0,0 +1,41 @@
+﻿//{{NO_DEPENDENCIES}}
+// Включаемый файл, созданный в Microsoft Visual C++.
+// Используется lab6.rc
+//
+#define _APS_NO_MFC                     1
+#define IDC_MYICON                      2
+#define IDD_LAB6_DIALOG                 102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDC_POINTER_COPY                105
+#define IDC_NODROP                      106
+#define IDI_LAB6                        107
+#define IDI_SMALL                       108
+#define IDC_LAB6                        109
+#define _APS_NEXT_SYMED_VALUE           110
+#define IDR_MAINFRAME                   128
+#define IDI_ICON1                       134
+#define IDI_ICON2                       135
+#define IDC_CURSOR1                     136
+#define IDI_ICON3                       137
+#define _APS_NEXT_RESOURCE_VALUE        140
+#define IDC_CURSOR2                     143
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define ID_CONTROL_START                32771
+#define ID_CONTROL_STOP                 32772
+#define _APS_NEXT_COMMAND_VALUE         32773
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        144
+#define _APS_NEXT_COMMAND_VALUE         32773
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
